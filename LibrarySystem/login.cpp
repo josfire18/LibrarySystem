8da6f106a448eef4aa41fe2e46d253218d15a75d
slstein14 <slstein14@my.trine.edu>
2017-03-23 13:53:33 -0400

    Prints name and pwd

--- LibrarySystem/login.cpp
+++ LibrarySystem/login.cpp
@@ -1,5 +1,7 @@
 #include "login.h"
 #include "ui_login.h"
+#include <qstring.h>
+#include <qdebug.h>
 
 Login::Login(QWidget *parent) :
     QDialog(parent),
@@ -15,7 +17,9 @@ Login::~Login()
 
 void Login::on_buttonBox_accepted()
 {
-
+    QString uname= ui->Username->text();
+    QString pwd=ui->Password->text();
+    qDebug()<<"User: "<<uname<<" Pwd: "<< pwd;
 }
 
 void Login::on_buttonBox_rejected()
