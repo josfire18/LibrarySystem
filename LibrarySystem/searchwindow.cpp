2b53ce6ca9bf71e98236ae1120336963fb56d607
unknown <jarockwood14@221MNFA06.tristate.edu>
2017-03-25 13:02:32 -0400

    Some book search stuff

--- LibrarySystem/searchwindow.cpp
+++ LibrarySystem/searchwindow.cpp
@@ -1,6 +1,22 @@
 #include "searchwindow.h"
 #include "ui_searchwindow.h"
 #include "QMessageBox"
+#include "iostream"
+#include "cstdlib"
+#include "fstream"
+#include "sstream"
+#include "string"
+using namespace std;
+
+
+//Struct for containing the address in 5 separate parts
+struct book{
+    string title;
+    string author;
+    string isbn;
+    string totNum;
+    string inStock;
+};
 
 SearchWindow::SearchWindow(QWidget *parent) :
     QWidget(parent),
@@ -16,6 +32,36 @@ SearchWindow::~SearchWindow()
 
 void SearchWindow::on_btnSearch_clicked()
 {
+    string bookList = "booklist.txt";
+    //Open file
+    ifstream file;
+    file.open(bookList.c_str());
+    book arr[1010];
+
+    //Find File Size
+    int arrSize;
+    arrSize = lineCount(bookList);
+
+    if (file.is_open()) {
+        cout << endl << "File Opened Sucessfully \n" << endl;
+        string line;
+        while (file.good())
+        {
+            for (int i = 1; i <= arrSize; i++) {
+                getline(file, line, '|');
+                arr[i].title = line;
+                getline(file, line, '|');
+                arr[i].author = line;
+                getline(file, line, '|');
+                arr[i].isbn = line;
+                getline(file, line, '|');
+                arr[i].totNum = line;
+                getline(file, line);
+                arr[i].inStock = line;
+            }
+        }
+
+        file.close();
     //Check if search box is empty
     if(ui->txtSearch->text() == "")
     {
@@ -23,5 +69,7 @@ void SearchWindow::on_btnSearch_clicked()
         errorMessage.setText("Please enter something to search.");
         errorMessage.exec();
     }
+
+
 
 }
--- build-LibrarySystem-Desktop_Qt_5_4_2_MinGW_32bit-Debug/ui_searchwindow.h
+++ build-LibrarySystem-Desktop_Qt_5_4_2_MinGW_32bit-Debug/ui_searchwindow.h
@@ -29,27 +29,43 @@ public:
     QLineEdit *txtSearch;
     QPushButton *btnSearch;
     QLabel *lblSearch;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
 
     void setupUi(QWidget *SearchWindow)
     {
         if (SearchWindow->objectName().isEmpty())
             SearchWindow->setObjectName(QStringLiteral("SearchWindow"));
-        SearchWindow->resize(400, 300);
+        SearchWindow->resize(835, 555);
         txtSearchResults = new QTextBrowser(SearchWindow);
         txtSearchResults->setObjectName(QStringLiteral("txtSearchResults"));
-        txtSearchResults->setGeometry(QRect(10, 90, 371, 192));
+        txtSearchResults->setGeometry(QRect(10, 120, 811, 421));
         txtSearch = new QLineEdit(SearchWindow);
         txtSearch->setObjectName(QStringLiteral("txtSearch"));
-        txtSearch->setGeometry(QRect(10, 60, 281, 21));
+        txtSearch->setGeometry(QRect(210, 60, 281, 21));
         btnSearch = new QPushButton(SearchWindow);
         btnSearch->setObjectName(QStringLiteral("btnSearch"));
-        btnSearch->setGeometry(QRect(290, 60, 91, 23));
+        btnSearch->setGeometry(QRect(490, 60, 91, 23));
         lblSearch = new QLabel(SearchWindow);
         lblSearch->setObjectName(QStringLiteral("lblSearch"));
-        lblSearch->setGeometry(QRect(90, 10, 311, 31));
+        lblSearch->setGeometry(QRect(300, 10, 311, 31));
         QFont font;
         font.setPointSize(18);
         lblSearch->setFont(font);
+        label = new QLabel(SearchWindow);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(40, 100, 47, 13));
+        label_2 = new QLabel(SearchWindow);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(150, 100, 47, 13));
+        label_3 = new QLabel(SearchWindow);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(280, 100, 47, 13));
+        label_4 = new QLabel(SearchWindow);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(440, 100, 47, 13));
 
         retranslateUi(SearchWindow);
 
@@ -61,6 +77,10 @@ public:
         SearchWindow->setWindowTitle(QApplication::translate("SearchWindow", "Form", 0));
         btnSearch->setText(QApplication::translate("SearchWindow", "Search", 0));
         lblSearch->setText(QApplication::translate("SearchWindow", "Local Library Search", 0));
+        label->setText(QApplication::translate("SearchWindow", "Title", 0));
+        label_2->setText(QApplication::translate("SearchWindow", "Author", 0));
+        label_3->setText(QApplication::translate("SearchWindow", "ISBN", 0));
+        label_4->setText(QApplication::translate("SearchWindow", "# Avail", 0));
     } // retranslateUi
 
 };
